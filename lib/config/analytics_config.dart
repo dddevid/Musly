cfca53a3d5882c0fd10b3d14c760c538c4ecaa7b
dddevid <141579210+dddevid@users.noreply.github.com>
2026-09-19 12:25:48 +0200

    chore: remove unused files and dependencies

--- lib/config/analytics_config.dart
+++
@@ -1,7 +0,0 @@
-class AnalyticsConfig {
-  static const String serverUrl = '';
-  static const String appKey = '';
-
-  static bool get isConfigured => false;
-  static String get maskedAppKey => '';
-}
--- lib/utils/utils.dart
+++
@@ -1,6 +0,0 @@
-export 'context_extensions.dart';
-export 'formatters.dart';
-export 'image_cache.dart';
-export 'navigation_helper.dart';
-export 'screen_helper.dart';
-export 'ui_feedback.dart';
